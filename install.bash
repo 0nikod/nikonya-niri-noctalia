e076328046f54caf257d1eaef438f2b0f1126b9a
0nikod <dniko0@outlook.com>
2026-07-20 22:13:37 +0800

    add niri-portals

--- install.bash
+++ install.bash
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+sudo -v || exit 1
+
+sudo chown -R root:root ./etc
+sudo stow --target="/" --verbose etc
+
 stow --target="$HOME" --verbose fastfetch
 stow --target="$HOME" --verbose niri
 stow --target="$HOME" --verbose noctalia
+
